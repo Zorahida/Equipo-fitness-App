88b3ddf4fac5bc3fe79f2fa11c4c793473a32c52
lorenaCoPi <124461594+lorenaCoPi@users.noreply.github.com>
2023-07-22 12:14:29 +0200

    Login provisional

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import "./styles/App.css";
-import { Route, Routes } from "react-router-dom";
+import userList from "../src/data/userList.json";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import NavBar from "./components/Nav/Nav";
-//import Home from "./components/Home/Home";
-//import Register from "./components/Register/Register";
-//import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 //import PersonalArea from "./components/PersonalArea/PersonalArea";
 import Contact from "./components/Contact/Contact";
 import About from "./components/About/About";
 import NotFound from "./components/NotFound/NotFound";
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState(null);
+  const [loginError, setLoginError] = useState("");
+
+  const loginUser = (formData, prevRoute) => {
+    const existsUser = userList.users.find(
+      (user) =>
+        user.email === formData.email && user.password === formData.password
+    );
+    if (existsUser) {
+      setUser(existsUser);
+      setLoginError("");
+      navigate(prevRoute || "/");
+    } else {
+      setUser(false);
+      setLoginError("Usuario o contraseña incorrectos");
+    }
+  };
+
+  const logoutUser = () => {
+    setUser(null);
+    navigate("/");
+  };
+
   return (
     <>
       <div className="App">
@@ -17,12 +45,12 @@ function App() {
       </div>
 
       <div className="App-header">
-        <NavBar />
+        <NavBar user={user} logoutUser={logoutUser}/>
         <Routes>
-         {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<PersonalArea />} />
+          <Route path="/login" element={<Login loginUser={loginUser} loginError={loginError}/>} />
+          {/*<Route path="/profile" element={<PersonalArea />} />
           <Route path="/training" element={<TrainingList />}/> ----> Pendiente de crear el componente*/}
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
