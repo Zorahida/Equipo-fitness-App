705fed2783f41fbf1c4491a6a30e7b6cf803e021
Andreu Rodríguez Lajarín <andreurl97@gmail.com>
2023-07-22 18:19:13 +0200

    Footer 0.0

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
+// const logo = require("")
+const social1 = require("../../assets/images/instagram.png")
+const social2 = require("../../assets/images/facebook-icon-white-png-1200x1200.png")
+const social3 = require("../../assets/images/twitter.png")
+
 const Footer = () => {
   return (
-    <nav>
-      <ul className="footer">
+    <nav className="footer_nav">
+      <ul>
         {/* <li>
           <Link to="/">
-            <img className="footer_img" src="" alt="" />
+          <img className="footer_img" src={logo} alt="" />
           </Link>
         </li>
         <li>
@@ -32,11 +37,14 @@ const Footer = () => {
         </li>
         <li>
           <Link to="/about">Sobre nosotros</Link>
-        </li>
+         </li> */}
         <li>
           <button className="logout">Logout</button>
-        </li> */}
+        </li>
       </ul>
+      <a href="#"><img className="footer_img" src={social1} alt=""></img></a>
+      <a href="#"><img className="footer_img" src={social2} alt=""></img></a>
+      <a href="#"><img className="footer_img" src={social3} alt=""></img></a>
       <p>
         ©2023 Gonzalo Cano | Lorena Cobo | Zorahida Domínguez | Lorena Cobo |
         Augusto Suñen | Andreu Rodríguez | All Rights Reserved
