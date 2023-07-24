4d1f1f1d9d873310be5bf08e9717f6e1473537bf
Andreu Rodríguez Lajarín <andreurl97@gmail.com>
2023-07-24 23:41:11 +0200

    Primer diseño de las tarjetas

--- src/components/TrainingList/TrainingList.jsx
+++ src/components/TrainingList/TrainingList.jsx
@@ -1,23 +1,33 @@
-const TrainingList = ({trainings}) => {
+import "./TrainingList.css";
+
+const tick = require("../../assets/images/checked.png")
+
+const TrainingList = ({ trainings }) => {
     const renderTrainings = () => {
-        return trainings.map((training) => <article className="training_card" key={training.id}>
-            <img className="training_img" src={training.image}></img>
-            <h4>{training.name}</h4>
-            <h5>{training.muscles}</h5>
-            <p>{training.description}</p>
-            <p>Dificultad: {training.level}</p>
-            <p>Series: {training.series}</p>
-            <p>Repeticiones: {training.reps}</p>
-            <iframe>{training.video}</iframe>
+    return trainings.map((training) => (
+      <article className="section_training_card" key={training._id}>
+        <h2>{training.nombre}</h2>
+        <iframe
+        className="video"
+          height="315"
+          src={training.video}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+        {training.musculos.map((musculo, id) => {
+                        return <span className="span" key={id}><img className="tick" src={tick} alt="tick"></img>{musculo}</span>
+                    })}
+        <p className="description">{training.descripcion}</p>
+        <p className="element"><strong>Dificultad:</strong> {training.dificultad}</p>
+        <p className="element"><strong>Series:</strong> {training.series}</p>
+        <p className="element"><strong>Repeticiones:</strong> {training.repeticiones}</p>
+      </article>
+    ));
+  };
 
-        </article>)
-    }
-
-    return(
-        <section className="section">
-        {renderTrainings()}
-        </section>
-    )
-}
+  return <section className="section">{renderTrainings()}</section>;
+};
 
 export default TrainingList;
