2f76e072d5b79ed74a6a94b9bf1289fcc5300143
demilv <gonzalo.cano@bootcamp-upgrade.com>
2023-07-27 20:37:53 +0200

    Campos

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -17,8 +17,13 @@ const Register = () => {
   const result = async (data) => {
     console.log(data);
     try{
-    const res = await axios(
-      "https://proyect-back-final-olive.vercel.app/newUser");
+      data.peso = parseInt(data.peso);
+      data.objetivo = parseInt(data.objetivo);
+      data.altura = parseInt(data.altura);
+      data.edad = parseInt(data.edad);
+      data.role = "user"
+    const res = await axios.post(      
+      "https://proyect-back-final-olive.vercel.app/usuariosBase/newUser");
       console.log("Response:", res.data);
     } catch (error){
       console.error("Error ocurred:", error.message);
@@ -30,30 +35,30 @@ const Register = () => {
         <h1 className="register_title"> Formulario de registro </h1>
         <form className="register_form" onSubmit={handleSubmit(result)}>
           <input
-            class="register_input"
-            typeclass="text"
+            className="register_input"
+            type="text"
             placeholder="Nombre"
-            {...register("Nombre", {
+            {...register("nombre", {
               required: "El nombre es requerido.",
             })}
           />
           {errors.Nombre && <p>{errors.Nombre.message}</p>}
 
           <input
-            class="register_input"
-            typeclass="text"
-            placeholder="Apellido"
-            {...register("Apellido", {
-              required: "El Apellido es requerido.",
+            className="register_input"
+            type="text"
+            placeholder="username"
+            {...register("username", {
+              required: "El Username es requerido.",
             })}
           />
-          {errors.Apellido && <p>{errors.Apellido.message}</p>}
+          {errors.Username && <p>{errors.Username.message}</p>}
 
           <input
-            class="register_input"
-            typeclass="text"
+            className="register_input"
+            type="email"
             placeholder="Mail"
-            {...register("mail", {
+            {...register("correo", {
               required: "El mail es requerido.",
               pattern: {
                 message: "No cumple los requisitos",
@@ -65,20 +70,60 @@ const Register = () => {
           {errors.mail && <p>{errors.mail.message}</p>}
 
           <input
-            class="register_input"
-            typeclass="number"
+            className="register_input"
+            type="text"
             placeholder="Edad"
-            {...register("Edad", {
+            {...register("edad", {
               required: "La edad es necesario.",
             })}
           />
           {errors.Edad && <p>{errors.Edad.message}</p>}
 
           <input
-            class="register_input"
-            typeclass="text"
+            className="register_input"
+            type="text"
+            placeholder="Peso"
+            {...register("peso", {
+              required: "El peso es necesario.",
+            })}
+          />
+          {errors.Peso && <p>{errors.Peso.message}</p>}
+
+          <input
+            className="register_input"
+            type="text"
+            placeholder="Objetivo"
+            {...register("objetivo", {
+              required: "La objetivo es necesario.",
+            })}
+          />
+          {errors.Objetivo && <p>{errors.Objetivo.message}</p>}
+
+          <input
+            className="register_input"
+            type="text"
+            placeholder="Altura"
+            {...register("altura", {
+              required: "La altura es necesario.",
+            })}
+          />
+          {errors.Altura && <p>{errors.Altura.message}</p>}
+
+          <input
+            className="register_input"
+            type="text"
+            placeholder="Genero"
+            {...register("genero", {
+              required: "El genero es necesario.",
+            })}
+          />
+          {errors.Genero && <p>{errors.Genero.message}</p>}
+
+          <input
+            className="register_input"
+            type="password"
             placeholder="Contraseña"
-            {...register("Contraseña", {
+            {...register("password", {
               required: "La contraseña es necesaria.",
               pattern: {
                 message:
